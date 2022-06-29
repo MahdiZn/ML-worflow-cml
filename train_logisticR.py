856bb2131f3d83d8202fed8326262ae5a9e4d4da
mahdizn <lmahdizouine@gmail.com>
2022-06-29 12:30:46 +0200

    add train model

--- train_logisticR.py
+++ train_logisticR.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
+import pickle
 from sklearn.model_selection import StratifiedShuffleSplit ,GridSearchCV
 from sklearn.linear_model import LogisticRegression,LogisticRegressionCV
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, precision_score, f1_score, roc_auc_score
@@ -18,13 +20,11 @@ data=pd.read_csv('clean_Rain_austrialia.csv')
 # * Split the data into train and test data sets. This can be done using any method, but consider using Scikit-learn's `StratifiedShuffleSplit` to maintain the same ratio of predictor classes.
 # 
 
-# In[77]:
 
 
 data.RainTomorrow.value_counts(normalize=True)
 
 
-# In[79]:
 
 
 feature_cols =  [x for x in data.columns  if  x not in 'RainTomorrow']
@@ -68,119 +68,29 @@ y_test.value_counts(normalize=True)
 lr = LogisticRegression(solver='liblinear')
 lr.fit(X_train, y_train)
 
-
-# In[84]:
-
-
-import pickle
 pkl_filename='logisticRegression.pkl'
 with open(pkl_filename,'wb') as file:
     pickle.dump(lr,file)
 
-
-# In[83]:
-
-
-lr_l1=LogisticRegressionCV(cv=4,solver='liblinear',penalty='l1')
-lr_l1.fit(X_train,y_train)
+with open ('logisticRegression.pkl','rb') as file :
+    lr=pickle.load(file)
 
 
-# In[85]:
+pred_train=lr.predict(X_train)
+score_train=accuracy_score(pred_train,y_train)
+pred_test=lr.predict(X_test)
+score_test=accuracy_score(pred_test,y_test)
+print(f"score_tarin:{score_train}")
+print(f"score_test:{score_test}")
 
-
-import pickle
-pkl_filename='lr_l1Cv.pkl'
-with open(pkl_filename,'wb') as file:
-    pickle.dump(lr_l1,file)
-
-
-#  __Compare models__
-# 
-
-# In[86]:
-
-
-from sklearn.metrics import precision_recall_fscore_support,accuracy_score as score
-
-pred_list=[]
-
-model=[lr,lr_l1]
-label=['lr','lr_l1']
-
-for lab,mod in zip(label,model):
-        pred_list.append(pd.Series(mod.predict(X_test),name=lab))
-        
-   # metric_list.append(pd.Series(accuracy_score(y_test,mod.predict(X_test)),name=lab))
-   # metric_list.append(pd.Series(precision_score(y_test,mod.predict(X_test)),name=lab))
+#write metrics to.txxt
+with open('metricas.txt','w') as file:
+    file.write(f"Training accuracy :{score_train}\n")
+    file.write(f"Test accuracy :{score_test}\n")
     
     
-
-pred_list = pd.concat(pred_list, axis=1)  
-
-
-pred_list.head()
-
-
-# In[87]:
-
-
-from sklearn.metrics import precision_recall_fscore_support as score
-
-
-metrics = list()
-
-cm= dict()
-for lab in label:
-
-    # Preciision, recall, f-score from the multi-class support function
-    precision, recall, fscore, _ = score(y_test, pred_list[lab],average='weighted')
-    
-    # The usual way to calculate accuracy
-    accuracy = accuracy_score(y_test, pred_list[lab])
-    
-    
-    # Last, the confusion matrix
-    cm[lab] = confusion_matrix(y_test, pred_list[lab])
-    
-    metrics.append(pd.Series({'precision':precision, 'recall':recall, 
-                              'fscore':fscore, 'accuracy':accuracy},
-                              
-                             name=lab))
-
-metrics = pd.concat(metrics, axis=1)
-
-
-# In[88]:
-
-
-metrics
-
-
-# In[90]:
-
-
-result_metrics = metrics.to_json(orient="columns")
-
-
-# In[92]:
-
-
-import json
-with open("metrics.json", 'w') as outfile:
-        json.dump(result_metrics, outfile)
-		
-		
-
-fig, axList = plt.subplots(nrows=2, ncols=2)
-axList = axList.flatten()
-fig.set_size_inches(12, 10)
-
-axList[-1].axis('off')
-
-for ax,lab in zip(axList[:-1], label):
-    sns.heatmap(cm[lab], ax=ax, annot=True, fmt='d', cmap='viridis');
-    ax.set(title=lab);
-    
-plt.tight_layout()
-plt.savefig("CM_ALL_LR.png",dpi=80)
+plt.figure(figsize=(7,7))
+cm=confusion_matrix(y_test,pred_test)
+sns.heatmap(cm, annot=True, fmt='d', cmap='viridis')
+plt.savefig('logistic.png',dpi=80)    
 ### END SOLUTION		
